0546df8030e1628c818628c7cfbf217aec73fd7c
David Bland <50449498+DavidABland@users.noreply.github.com>
2023-08-04 11:50:08 +0100

    Added updates to Engineers Report for 2 additional Tabs

---
+++ EngineersReport.js
@@ -0,0 +1,49 @@
+function ShowHideChecklist(executionContext) {
+	
+	var formContext = executionContext.getFormContext();
+	
+	var ChecklistType = formContext.getAttribute("tal_checklisttype").getValue();
+	
+   if(ChecklistType === 805430000) {
+       formContext.ui.tabs.get("Horizontal").setVisible(true);
+	   formContext.ui.tabs.get("Vertical").setVisible(false);
+	   formContext.ui.tabs.get("Compactor").setVisible(false);
+	   formContext.ui.tabs.get("RefurbAssess").setVisible(false);
+	   formContext.ui.tabs.get("RefurbInspect").setVisible(false);
+   }
+   else if(ChecklistType === 805430001) {
+       formContext.ui.tabs.get("Horizontal").setVisible(false);
+	   formContext.ui.tabs.get("Vertical").setVisible(true);
+	   formContext.ui.tabs.get("Compactor").setVisible(false);
+	   formContext.ui.tabs.get("RefurbAssess").setVisible(false);
+	   formContext.ui.tabs.get("RefurbInspect").setVisible(false);
+   }
+   else if(ChecklistType === 805430002) {
+       formContext.ui.tabs.get("Horizontal").setVisible(false);
+	   formContext.ui.tabs.get("Vertical").setVisible(false);
+	   formContext.ui.tabs.get("Compactor").setVisible(true);
+	   formContext.ui.tabs.get("RefurbAssess").setVisible(false);
+	   formContext.ui.tabs.get("RefurbInspect").setVisible(false);
+   }
+   else if (ChecklistType === 805430004) {
+	   formContext.ui.tabs.get("Horizontal").setVisible(false);
+	   formContext.ui.tabs.get("Vertical").setVisible(false);
+	   formContext.ui.tabs.get("Compactor").setVisible(false);
+	   formContext.ui.tabs.get("RefurbAssess").setVisible(true);
+	   formContext.ui.tabs.get("RefurbInspect").setVisible(false);
+   }
+   else if (ChecklistType === 805430005) {
+	   formContext.ui.tabs.get("Horizontal").setVisible(false);
+	   formContext.ui.tabs.get("Vertical").setVisible(false);
+	   formContext.ui.tabs.get("Compactor").setVisible(false);
+	   formContext.ui.tabs.get("RefurbAssess").setVisible(false);
+	   formContext.ui.tabs.get("RefurbInspect").setVisible(true);
+   }
+   else {
+	   formContext.ui.tabs.get("Horizontal").setVisible(false);
+	   formContext.ui.tabs.get("Vertical").setVisible(false);
+	   formContext.ui.tabs.get("RefurbAssess").setVisible(false);
+	   formContext.ui.tabs.get("RefurbInspect").setVisible(false);
+	   formContext.ui.tabs.get("Compactor").setVisible(false);
+   }
+}
