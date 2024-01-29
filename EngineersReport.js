8cd1200e7413c7b833d5404b3605c69fb4b84ab2
David Bland <50449498+DavidABland@users.noreply.github.com>
2024-01-29 12:46:16 +0000

    Updates for Action Complete - not complete

--- EngineersReport.js
+++ EngineersReport.js
@@ -61,3 +61,61 @@ function ShowHideChecklist(executionContext) {
 	   formContext.ui.tabs.get("RefurbCombined").setVisible(false);
    }
 }
+
+
+function ShowHideActionComplete(executionContext) {
+
+	//alert("ShowHideActionComplete");
+
+	var formContext = executionContext.getFormContext();
+
+	var ChecklistType = formContext.getAttribute("tal_checklisttype").getValue();
+
+	if (ChecklistType === 805430006) {
+
+		if (formContext.getAttribute("tal_preusedpicturestaken").getValue() === 805430001) { // No from Yes/ NO /N/A
+			formContext.getControl("tal_completed01").setVisible(true);
+			formContext.getControl("tal_engineerwhocompleted01").setVisible(true);
+		}
+		else {
+			formContext.getControl("tal_completed01").setVisible(false);
+			formContext.getControl("tal_engineerwhocompleted01").setVisible(false);
+		}
+	}
+
+	if (ChecklistType === 805430006) {
+
+		if (formContext.getAttribute("tal_preusedpicturestaken").getValue() === 805430001) { // No from Yes/ NO /N/A
+			formContext.getControl("tal_completed02").setVisible(true);
+			formContext.getControl("tal_engineerwhocompleted02").setVisible(true);
+		}
+		else {
+			formContext.getControl("tal_completed02").setVisible(false);
+			formContext.getControl("tal_engineerwhocompleted02").setVisible(false);
+		}
+	}
+}
+
+function SetEngineer(executionContext, Number) {
+
+	//alert("ShowHideActionComplete");
+
+	var formContext = executionContext.getFormContext();
+
+	var userSettings = Xrm.Utility.getGlobalContext().userSettings;
+
+	var currentuserid = userSettings.userId;
+	var username = userSettings.userName;
+
+	var FieldValue = formContext.getAttribute("tal_completed" + Number).getValue();
+
+	//alert(FieldValue);
+
+	if (FieldValue === true) {
+		formContext.getAttribute("tal_engineerwhocompleted" + Number).setValue(username);
+	}
+	else {
+		formContext.getAttribute("tal_engineerwhocompleted" + Number).setValue("");
+	}
+}
+
